ecaf72adc20171e14313d65dad9d5b922013d54d
Adrian Winterstein <dev@cerritus.eu>
2018-05-12 20:04:08 +0200

    Added a function to sync additional libraries from a Raspberry Pi.

--- templates/build-rpi-toolchain.sh
+++ templates/build-rpi-toolchain.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+function sync_additionals_into_sysroot() {
+
+    RASPI_ADDRESS=${RASPI_ADDRESS:="{{raspi_address}}"}
+    RASPI_USER=${RASPI_USER:="{{raspi_user}}"}
+    SYSROOT_DIR="{{toolchains_dir}}/{{toolchain_prefix}}/{{toolchain_prefix}}/sysroot/"
+
+    rsync -avzR $RASPI_USER@$RASPI_ADDRESS:/usr/include/boost \
+                                          :/usr/include/spdlog \
+                                          :/usr/include/mosquitto.h \
+                                          :/usr/lib/arm-linux-gnueabihf/libssl* \
+                                          :/usr/lib/arm-linux-gnueabihf/libcrypto* \
+                                          :/usr/lib/arm-linux-gnueabihf/libcares* \
+                                          :/usr/lib/arm-linux-gnueabihf/libboost_* \
+                                          :/usr/lib/arm-linux-gnueabihf/libmosquitto.* \
+                                          "$SYSROOT_DIR"
+
+    # create symlinks so that the libraries can be used without multilib
+    pushd "${SYSROOT_DIR}/usr/lib/" >/dev/null
+    ls "arm-linux-gnueabihf/" | while read filename; do
+        if [ ! -e "$filename" ]; then
+            ln -s "arm-linux-gnueabihf/$filename" "$filename"
+        fi
+    done
+    popd >/dev/null
+}
+
+
 cd "{{toolsdir}}"
 
 if [ ! -e "crosstool-ng" ]; then
@@ -22,6 +49,8 @@ fi
 
 mkdir -p "{{tarballs_dir}}"
 ./ct-ng build
+
+sync_additionals_into_sysroot
 
 pushd "{{toolchains_dir}}" >/dev/null
 tar cfj "{{homedir}}/toolchain_{{toolchain_prefix}}.tbz2" "{{toolchain_prefix}}"
