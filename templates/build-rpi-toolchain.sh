9d93554614d583d0a35fee9c0f4c406d22718850
Adrian Winterstein <dev@cerritus.eu>
2018-06-15 21:21:20 +0200

    Sync more libraries that are needed for compiling Qt applications.

--- templates/build-rpi-toolchain.sh
+++ templates/build-rpi-toolchain.sh
@@ -6,24 +6,42 @@ function sync_additionals_into_sysroot() {
     RASPI_USER=${RASPI_USER:="{{raspi_user}}"}
     SYSROOT_DIR="{{toolchains_dir}}/{{toolchain_prefix}}/{{toolchain_prefix}}/sysroot/"
 
+    chmod u+w "$SYSROOT_DIR"
+    mkdir -p "$SYSROOT_DIR/opt"
+
     rsync -avzR $RASPI_USER@$RASPI_ADDRESS:/usr/include/boost \
                                           :/usr/include/spdlog \
                                           :/usr/include/mosquitto.h \
+                                          :/usr/lib/arm-linux-gnueabihf/libz.* \
                                           :/usr/lib/arm-linux-gnueabihf/libssl* \
                                           :/usr/lib/arm-linux-gnueabihf/libcrypto* \
                                           :/usr/lib/arm-linux-gnueabihf/libcares* \
                                           :/usr/lib/arm-linux-gnueabihf/libboost_* \
                                           :/usr/lib/arm-linux-gnueabihf/libmosquitto.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libdouble-conversion.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libpng16.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libharfbuzz.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libglib-2.0.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libfreetype.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libgraphite2.* \
+                                          :/usr/lib/arm-linux-gnueabihf/libpcre.* \
+                                          :/lib/arm-linux-gnueabihf/libz.* \
+                                          :/lib/arm-linux-gnueabihf/libglib-2.0.* \
+                                          :/lib/arm-linux-gnueabihf/libpcre.* \
+                                          :/opt/vc/lib/* \
                                           "$SYSROOT_DIR"
 
-    # create symlinks so that the libraries can be used without multilib
-    pushd "${SYSROOT_DIR}/usr/lib/" >/dev/null
-    ls "arm-linux-gnueabihf/" | while read filename; do
-        if [ ! -e "$filename" ]; then
-            ln -s "arm-linux-gnueabihf/$filename" "$filename"
-        fi
-    done
-    popd >/dev/null
+    # write LD config file for the sysroot
+    mkdir -p "$SYSROOT_DIR/etc/"
+    chmod u+w "$SYSROOT_DIR/etc/"
+    touch "$SYSROOT_DIR/etc/ld.so.conf"
+cat << EOF > "$SYSROOT_DIR/etc/ld.so.conf"
+/opt/vc/lib
+/lib/arm-linux-gnueabihf
+/usr/lib/arm-linux-gnueabihf
+/usr/local/lib
+EOF
+
 }
 
 
